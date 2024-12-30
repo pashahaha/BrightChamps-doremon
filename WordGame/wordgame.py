86b991cb4ac42d16ae6855fd775444b4c65324df
pashahaha <irfanjunianpasha8@gmail.com>
2024-12-30 16:26:29 +0700

    Update of project

---
+++ WordGame/wordgame.py
@@ -0,0 +1,17 @@
+import random
+
+words = ['Aardvark', 'Bubble', 'Cactus', 'Dolphin', 'Eagle']
+
+word = random.choice(words)
+
+name = input("What is your name? ")
+
+guessedLetters = ''
+
+chance = 10
+
+print("Hello " + name + "! Let's play a game of word guess")
+while chance > 0:
+    print("You have " + str(chance) + " chances left")
+    chance = chance - 1
+    guess = input("Guess a letter: ")
